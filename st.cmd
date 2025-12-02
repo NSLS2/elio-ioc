42679fca5d83f0ea7287ceab67e0084a81dc2403
Huijuan Xu <hxu@bnl.gov>
2025-12-02 10:49:09 -0500

    add io modules in test box

--- st.cmd
+++ st.cmd
@@ -5,7 +5,7 @@
 
 #< envPaths
 epicsEnvSet("DEVICE","EK9K1")
-epicsEnvSet("NUMTERM",9)
+epicsEnvSet("NUMTERM",11)
 epicsEnvSet("Sys", "XF:31ID1-ES")
 epicsEnvSet("Dev", "GPIO:1")
 epicsEnvSet("TOP","/nsls2/users/hxu/iocs/elio-ioc")
@@ -15,70 +15,85 @@ dbLoadDatabase("dbd/elio.dbd",0,0)
 elio_registerRecordDeviceDriver(pdbbase) 
 
 ek9000Configure("$(DEVICE)", "10.69.58.101", 502, "$(NUMTERM)")
+#asynSetTraceMask("$(DEVICE)", 0, 9)
+#asynSetTraceIOMask("$(DEVICE)", 0, 9)
 
 cd "${TOP}/db/"
 
 dbLoadRecords("ek9000_status.db","P=$(Sys){$(Dev)},EK9K=$(DEVICE)")
 
-
-#Configure terminal 1: EL3068
+#Configure terminal 1: EL1808
 epicsEnvSet("POS",1)
+epicsEnvSet("MODEL","EL1808")
+epicsEnvSet("Dev","{GPIO:1_$(POS)}")
+ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
+dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
+
+#Configure terminal 2: EL2798
+epicsEnvSet("POS",2)
+epicsEnvSet("MODEL","EL2798")
+epicsEnvSet("Dev","{GPIO:1_$(POS)}")
+ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
+dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
+
+#Configure terminal 3: EL2808
+epicsEnvSet("POS",3)
+epicsEnvSet("MODEL","EL2808")
+epicsEnvSet("Dev","{GPIO:1_$(POS)}")
+ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
+dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
+
+#Configure terminal 4: EL3008
+epicsEnvSet("POS",4)
+epicsEnvSet("MODEL","EL3008")
+epicsEnvSet("Dev","{GPIO:1_$(POS)}")
+ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
+dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
+
+#Configure terminal 5: EL3068
+epicsEnvSet("POS",5)
 epicsEnvSet("MODEL","EL3068")
 epicsEnvSet("Dev","{GPIO:1_$(POS)}")
 ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
 dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
 
-#Configure terminal 2: EL4008
-epicsEnvSet("POS",2)
+#Configure terminal 6: EL3164
+epicsEnvSet("POS",6)
+epicsEnvSet("MODEL","EL3164")
+epicsEnvSet("Dev","{GPIO:1_$(POS)}")
+ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
+dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
+
+#Configure terminal 7: EL4008
+epicsEnvSet("POS",7)
 epicsEnvSet("MODEL","EL4008")
 epicsEnvSet("Dev","{GPIO:1_$(POS)}")
 ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
 dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
 
-#Configure terminal 3: EL1808
-epicsEnvSet("POS",3)
-epicsEnvSet("MODEL","EL1808")
-epicsEnvSet("Dev","{GPIO:1_$(POS)}")
-ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
-dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
-
-#Configure terminal 6: EL1808
-epicsEnvSet("POS",4)
-epicsEnvSet("MODEL","EL1808")
-epicsEnvSet("Dev","{GPIO:1_$(POS)}")
-ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
-dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
-
-#Configure terminal 5: EL2808
-epicsEnvSet("POS",5)
-epicsEnvSet("MODEL","EL2808")
-epicsEnvSet("Dev","{GPIO:1_$(POS)}")
-ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
-dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
-
-#Configure terminal 6: EL2808
-epicsEnvSet("POS",6)
-epicsEnvSet("MODEL","EL2808")
-epicsEnvSet("Dev","{GPIO:1_$(POS)}")
-ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
-dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
-
-#Configure terminal 7: EL3068
-epicsEnvSet("POS",7)
-epicsEnvSet("MODEL","EL3068")
-epicsEnvSet("Dev","{GPIO:1_$(POS)}")
-ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
-dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
-
-#Configure terminal 8: EL1124
+#Configure terminal 8: EL4038
 epicsEnvSet("POS",8)
+epicsEnvSet("MODEL","EL4038")
+epicsEnvSet("Dev","{GPIO:1_$(POS)}")
+ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
+dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
+
+#Configure terminal 9: EL9505
+epicsEnvSet("POS",9)
+epicsEnvSet("MODEL","EL9505")
+epicsEnvSet("Dev","{GPIO:1_$(POS)}")
+ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
+dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
+
+#Configure terminal 10: EL1124
+epicsEnvSet("POS",10)
 epicsEnvSet("MODEL","EL1124")
 epicsEnvSet("Dev","{GPIO:1_$(POS)}")
 ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
 dbLoadTemplate("$(MODEL).substitutions","DEVICE=$(DEVICE),Sys=$(Sys), Dev=$(Dev),POS=$(POS)")
 
-#Configure terminal 9: EL2124
-epicsEnvSet("POS",9)
+#Configure terminal 11: EL2124
+epicsEnvSet("POS",11)
 epicsEnvSet("MODEL","EL2124")
 epicsEnvSet("Dev","{GPIO:1_$(POS)}")
 ek9000ConfigureTerminal("$(DEVICE)", "$(Dev)", "$(MODEL)", "$(POS)")
